e382352b30a53cfacd40aee1c7a605dae7e58780
Jared Bruni <jaredbruni@gmail.com>
2026-02-07 11:08:18 -0800

    updated

---
+++ build/CMakeFiles/AcidDrop.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AcidDrop.dir/link.d"
+  "AcidDrop"
+  "AcidDrop.pdb"
+  "CMakeFiles/AcidDrop.dir/acid.drop.cpp.o"
+  "CMakeFiles/AcidDrop.dir/acid.drop.cpp.o.d"
+  "CMakeFiles/AcidDrop.dir/vk.cpp.o"
+  "CMakeFiles/AcidDrop.dir/vk.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AcidDrop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/AcidDrop.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for AcidDrop.
+# This may be replaced when dependencies are built.
